addbee7fc6b06c54f355ef47cdadcd632c6b63e6
Hassaan-Elahi <hassaan.elahi15@gmail.com>
2017-07-29 23:06:45 +0500

    added animations

--- source/Actions/index.js
+++ source/Actions/index.js
@@ -1,5 +1,8 @@
+import { View,ToastAndroid, Text, TouchableWithoutFeedback } from 'react-native';
+
 export const SelectLibrary = (libraryID) =>
 {
+    
     return {
 
     type: 'SelectLibrary',
--- source/components/listItem.js
+++ source/components/listItem.js
@@ -1,29 +1,84 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableWithoutFeedback } from 'react-native';
+
+import { View, 
+ToastAndroid,
+LayoutAnimation,
+Text, 
+TouchableWithoutFeedback,
+NativeModules
+ } from 'react-native';
+
 import { connect } from 'react-redux';
 import { CardSection } from './Common';
 import * as Actions from '../Actions';
 
 
+const dummy = NativeModules.UIManager.setLayoutAnimationEnabledExperimental &&
+NativeModules.UIManager.setLayoutAnimationEnabledExperimental(true);
+
 
 class ListItem extends Component
 {
 
+    
+    componentWillUpdate()
+    {
+      LayoutAnimation.spring();  
+    }
+
+    renderDesctiption()
+    {
+        if (this.props.selectionID === this.props.data.id)
+            {
+               
+                return <Text>{this.props.data.description }</Text>;
+            }
+
+    }
+
+    renderText()
+    {
+        if (this.props.data.id === this.props.selectionID)
+        {
+             return ({
+            fontSize: 18,
+            paddingLeft: 10,
+            color: 'black',
+            backgroundColor: '#c0dd92',
+            flex: 1
+
+            });
+        }
+        else 
+        {
+            return ({
+            fontSize: 18,
+            paddingLeft: 10,
+            color: 'black',
+
+            });
+
+        }    
+        
+    }
 
     render()
     {
+         
         return (
             <TouchableWithoutFeedback onPress={() => this.props.SelectLibrary(this.props.data.id)} >    
                 <View>
                     <CardSection>
 
-                        <Text style={styles.titleStyle}>
+                        <Text style={this.renderText()}>
 
                             { this.props.data.title }
 
                         </Text>
 
                     </CardSection>
+                    {this.renderDesctiption()}
+                    
                 </View>
             </TouchableWithoutFeedback>    
         );
@@ -44,5 +99,12 @@ const styles = {
   }
 };
 
-export default connect(null, Actions)(ListItem);
+const mapStateToPops = (state) =>
+{
+  
+    return { selectionID: state.selectionID };
+
+}
+
+export default connect(mapStateToPops, Actions)(ListItem);
 
